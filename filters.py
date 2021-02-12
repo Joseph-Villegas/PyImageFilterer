6012f445efe5e9338c042eb53a1d2f57ffbd76b8
Joseph-Villegas <jovillegas@csumb.edu>
2021-02-11 20:12:07 -0800

    added grayscale filter

--- filters.py
+++ filters.py
@@ -5,3 +5,6 @@ def invert(image):
 	# filtered_image.putdata(list(tuple(map(lambda pixel: (255 - pixel[0], 255 - pixel[1], 255 - pixel[2]), image.getdata()))))
 	filtered_image.putdata([(255 - pixel[0], 255 - pixel[1], 255 - pixel[2]) for pixel in image.getdata()])
 	return filtered_image
+
+def grayscale(image):     
+	return image.convert("L")
