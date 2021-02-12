3e2a6dd7a576d44ac240886c913d480bb60b019c
Joseph-Villegas <jovillegas@csumb.edu>
2021-02-11 20:11:34 -0800

    added invert filter

--- filters.py
+++ filters.py
@@ -1,1 +1,7 @@
 from PIL import Image
+
+def invert(image):
+	filtered_image = image.copy()
+	# filtered_image.putdata(list(tuple(map(lambda pixel: (255 - pixel[0], 255 - pixel[1], 255 - pixel[2]), image.getdata()))))
+	filtered_image.putdata([(255 - pixel[0], 255 - pixel[1], 255 - pixel[2]) for pixel in image.getdata()])
+	return filtered_image
