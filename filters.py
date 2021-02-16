3a77149f9ff5a3e9bb2993a76943de36e86f6d23
Joseph-Villegas <jovillegas@csumb.edu>
2021-02-15 16:30:06 -0800

    added sepia and contrast filters

--- filters.py
+++ filters.py
@@ -1,22 +1,54 @@
 from PIL import Image
 
-# NOTE: the 'image' parameter is a PIL image objects in all methods
+# NOTE: the 'image' parameter is a PIL image object in all methods
+
+""" Begin Helper Functions """
+
+def bound(value):
+	""" Enforces upper bound for color channels """
+	return 255 if value > 255 else int(value)
+
+def get_sepia_pixel(red, green, blue):
+	""" Returns a sepia toned pixel """
+	return (
+		bound((0.759 * red) + (0.398 * green) + (0.194 * blue)),
+		bound((0.676 * red) + (0.354 * green) + (0.173 * blue)),
+		bound((0.524 * red) + (0.277 * green) + (0.136 * blue))
+	)
+
+""" End Helper Functions """
 
 def invert(image):
+	""" Applies invert filter to a PIL image object """
 	filtered_image = image.copy()
 	# filtered_image.putdata(list(tuple(map(lambda pixel: (255 - pixel[0], 255 - pixel[1], 255 - pixel[2]), image.getdata()))))
 	filtered_image.putdata([(255 - pixel[0], 255 - pixel[1], 255 - pixel[2]) for pixel in image.getdata()])
 	return filtered_image
 
-def grayscale(image):     
+def grayscale(image):
+	""" Applies grayscale filter to a PIL image object """     
 	return image.convert("L")
 
 def swap_channels(image):
+	""" Applies swap channels filter to a PIL image object """
 	filtered_image = image.copy()
 	filtered_image.putdata([(pixel[2], pixel[0], pixel[1]) for pixel in image.getdata()])
 	return filtered_image
 
 def mask(image):
+	""" Applies mask filter to a PIL image object """
 	filtered_image = image.copy()
 	filtered_image.putdata([(0,)*3 if sum(pixel) / 3.0 < 128 else (255,)*3 for pixel in image.getdata()])
+	return filtered_image
+
+def sepia(image):
+	""" Applies sepia filter to a PIL image object """
+	filtered_image = image.copy()
+	filtered_image.putdata([get_sepia_pixel(pixel[0], pixel[1], pixel[2]) for pixel in image.getdata()])
+	return filtered_image
+
+def contrast(image):
+	""" Applies contrast filter to a PIL image object """
+	filtered_image = image.copy()
+	filtered_image.putdata([tuple(map(lambda value: bound(int(value / 2) if value < 128 else value * 2), pixel)) for pixel in image.getdata()])
 	return filtered_image
